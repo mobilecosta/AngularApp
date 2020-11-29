896e4f6516d59958ef9e12eeb76cc42e4cef6623
mobile.costa@gmail.com <mobile.costa@gmail.com>
2020-11-29 12:34:36 -0300

    Ajuste

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/angularapp'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angularapp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
